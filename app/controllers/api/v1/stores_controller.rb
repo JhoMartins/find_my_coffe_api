2e075316f4bca4a34ba66f0087dae5c0e0cfa03e
Jhonatan Martins <jhonatanbmartins@gmail.com>
2020-10-19 21:45:14 -0300

    creates store index view

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::StoresController < ApplicationController
   before_action :set_store, only: [:show]
 
+  def index
+    @stores = Store.within(params[:longitude].to_f, params[:latitude].to_f)
+                   .sort_by(&:ratings_average)
+                   .reverse
+  end
+
   def show; end
 
   private
